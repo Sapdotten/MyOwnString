a21fd89de4c49b4eb292d2fbbd8a4c7724b8eb9f
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-03-16 16:36:30 +0400

    конструкторы, size()

--- bonuscode/bonuscode.cpp
+++ bonuscode/bonuscode.cpp
@@ -1,15 +1,33 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 
-class String {
-public:
-    char a[9] = "fuck you";
+struct String {
+    char* _string; //8
     union {
-        float d;
-        double f;
-        const short int x = 8;
+        unsigned long long int _size=0;//8
+        char _short_string[24];//по 4 байта на 4 символа
     };
-    
+
+    String() : _size(0), _string(nullptr) {};
+    String(const char* str) {
+        if (strlen(str) < 24) {
+            strcpy(_short_string, str);
+            _string = _short_string;
+        }
+        else {
+            _size = strlen(str);
+            _string = new char[_size+1];
+            strcpy(_string, str);
+        }
+    }
+    unsigned long long size() {
+        if (_string==_short_string) {
+            return strlen(_string);
+        }
+        return _size;
+    }
+
 };
 
 
@@ -17,6 +35,7 @@ public:
 
 int main()
 {
-    String a;
-    std::cout << a.a<<sizeof(String);
+    String str1("Hello");
+    String str2;
+    cout << str1.size()<<endl<<str2.size();
 }
