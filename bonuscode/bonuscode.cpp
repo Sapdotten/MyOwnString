0552f9247e231d3351373f0609fe28afe2d372a2
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-03-16 15:25:45 +0400

    first init

---
+++ bonuscode/bonuscode.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+class String {
+public:
+    char a[9] = "fuck you";
+    union {
+        float d;
+        double f;
+        const short int x = 8;
+    };
+    
+};
+
+
+
+
+int main()
+{
+    String a;
+    std::cout << a.a<<sizeof(String);
+}
